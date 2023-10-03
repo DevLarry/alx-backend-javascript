72fe7ac7f3645be8f44c970ec10791144a8c6e1a
DevLarry <olanrewajusamad070@gmail.com>
2023-10-03 23:51:16 +0100

    Reduce and map array

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,12 @@
-export default function getStudentsByLocation (list, city, newGrades) {
+export default function getStudentsByLocation(list, city, newGrades) {
   if (Array.isArray(list) && Array.isArray(newGrades)) {
     return list
       .filter((e) => e.location === city)
       .map((e) => {
         return {
           ...e,
-          grade: newGrades.find((i) => i.studentId === e.id)?.grade || 'N/A'
+          grade: newGrades
+            .find((i) => i.studentId === e.id)?.grade || 'N/A'
         };
       });
   }
