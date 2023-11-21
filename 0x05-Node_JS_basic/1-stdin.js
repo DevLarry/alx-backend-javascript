0958173a8dec8357b2fa0434b86c244c6367a291
DevLarry <olanrewajusamad070@gmail.com>
2023-11-21 22:55:40 +0100

    Child processs

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,22 @@
 const readline = require('readline');
 
-console.log('Welcome to Holberton School, what is your name?');
-process.on('exit', () => {
-  console.log('This important software is now closing');
-});
+// console.log('Welcome to Holberton School, what is your name?');
+// process.on('exit', () => {
+//   console.log('This important software is now closing');
+// });
+
+// const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question('', (name) => {
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
   rl.close();
 });
+
 // const yourName = process.stdin.read(100);
 // console.log(`Your name is: ${yourName}`);
