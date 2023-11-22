59d30fad414442178556776f14d948667908d8c5
DevLarry <olanrewajusamad070@gmail.com>
2023-11-23 00:26:27 +0100

    Basic tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,17 @@
 const calculateNumber = require('./0-calcul');
 const assert = require('assert');
 
-describe('rounded', () => {
+describe('calculateNumber', () => {
   it('Should return the sum o thwo numbers', () => {
-    assert.equal(calculateNumber(12, 11), 23)
-    assert.equal(calculateNumber(-1, 11), 10)
+    assert.equal(calculateNumber(1, 3), 4)
+    assert.equal(calculateNumber(1, 3.7), 5)
     assert.equal(calculateNumber(0, 0), 0)
-    assert.equal(calculateNumber(-12, -11), -23)
+    assert.equal(calculateNumber(1.2, 3.7), 5)
+  })
+  it('Should return the sum of thwo numbers', () => {
+    assert.equal(calculateNumber(1, 3), 4)
+    assert.equal(calculateNumber(1, 3.7), 5)
+    assert.equal(calculateNumber(0, 0), 0)
+    assert.equal(calculateNumber(1.2, 3.7), 5)
   })
 })
