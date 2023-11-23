e4392d7fd29d7ef5510aff6f8ee85d82b1878981
DevLarry <olanrewajusamad070@gmail.com>
2023-11-24 00:14:47 +0100

    Basic tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,19 @@
-const calculateNumber = require("./0-calcul");
-const assert = require("assert");
+const calculateNumber = require('./0-calcul');
+const assert = require('assert');
 
-describe("calculateNumber", () => {
-  it("Should return the sum o thwo numbers", () => {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(0, 0), 0);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-  });
-  it("Should return the sum of thwo numbers", () => {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(0, 0), 0);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
+describe('calculateNumber', () => {
+  describe('calculate Number', () => {
+    it('Should return the sum o thwo numbers', () => {
+      assert.equal(calculateNumber(1, 3), 4);
+      assert.equal(calculateNumber(1, 3.7), 5);
+      assert.equal(calculateNumber(0, 0), 0);
+      assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
+    it('Should return the sum of thwo numbers', () => {
+      assert.equal(calculateNumber(1, 3), 4);
+      assert.equal(calculateNumber(1, 3.7), 5);
+      assert.equal(calculateNumber(0, 0), 0);
+      assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
   });
 });
